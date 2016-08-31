2954ba355cfdbdf2cfe73a480c6d40ff2def6444
Tuyen Le <ttle@iastate.edu>
2016-08-30 20:48:48 -0500

    revision for resubmission

--- 2nd_paper/figures/R studio graph/Rstudio_graph/term extraction precision.R
+++ 2nd_paper/figures/R studio graph/Rstudio_graph/term extraction precision.R
@@ -1,5 +1,8 @@
 read.csv('term extraction evaluation.csv')
 data=read.csv('term extraction evaluation.csv')
-x=data[c(1,3,7,10,13),1]
+sdata=data[1:6,c('Cvalue','precision')]
+show(sdata)
+x=data[c(1,2,3,4,5,6,7),1]
+show(x)
+barplot(sdata$precision,names=sdata$Cvalue, xlab="Cvalue", ylab="Precision(%)",ylim = c(0,100), las=2, mgp=c(3.1,1,0), cex.lab=1.2, cex.axis = 1.1, cex.names = 1.1)
 
-barplot(data$precision,names=data$Cvalue, xlab="Cvalue", ylab="Precision(%)")
--- re-submission/manuscript/figures/R studio graph/Rstudio_graph/term extraction precision.R
+++
@@ -1,5 +0,0 @@
-read.csv('term extraction evaluation.csv')
-data=read.csv('term extraction evaluation.csv')
-x=data[c(1,3,7,10,13),1]
-
-barplot(data$precision,names=data$Cvalue, xlab="Cvalue", ylab="Precision(%)")
